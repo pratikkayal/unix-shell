f394cd81737b8d26de42366a479f9754e973439f
Pratik <pratik.kayal@iitgn.ac.in>
2018-10-29 00:29:30 +0530

    new commands

--- myshell.c
+++ myshell.c
@@ -1,10 +1,10 @@
 #include<stdlib.h>
 #include<stdio.h>
 #include<string.h>
-#include <sys/stat.h>
+#include <sys/stat.h>   //mkdir
+#include <unistd.h>     //rmdir, pwd
 
 char * input;
-// char * input1;
 char* argval[10];
 int argcount = 0;
 int exitflag=0;
@@ -16,17 +16,14 @@ void getInput()
     ssize_t buf = 0;
     getline(&input,&buf,stdin);
     argcount = 0;
-    while((argval[argcount] = strsep(&input, " \t\n")) != NULL  && argcount < 9)
+    while((argval[argcount] = strsep(&input, " \t\n")) != NULL  && argcount < 9)    //strsep(char **stringp, const char *delim);
     {
+        // printf("START%sEND", argval[argcount]);
         if(sizeof(argval[argcount])==0)
         {
             free(argval[argcount]);
         }
         else argcount++;
-        if(strcmp(argval[argcount-1],"&")==0)
-        {
-            return;
-        }
     }
     free(input);
 }
@@ -46,6 +43,36 @@ void mkdir_func(char* name)
     
 }
 
+/* Remove a folder */
+void rmdir_func(char* name)
+{
+    int retCode = rmdir(name);
+    
+}
+
+void pwd_func(char* pwdpath,int printornot) // if printornot=1 --> print 
+{
+    char temp[1000];
+    char* path=getcwd(temp, sizeof(temp));
+    if(path != NULL)
+    {
+        strcpy(pwdpath,temp);
+        if(printornot==1)  
+        {
+            printf("%s\n",pwdpath);
+        }
+    }
+    else 
+    {
+
+    }
+}
+
+void clear()
+{
+    const char* blank = "\e[1;1H\e[2J";
+    write(STDOUT_FILENO,blank,12);
+}
 
 
 int main(int argc, char* argv[])
@@ -53,7 +80,11 @@ int main(int argc, char* argv[])
 	printf("Welcome to the new unix shell!!\n");
     while(exitflag==0)
     {
-    	printf(">>");
+        char temp[1000];
+        char* path=getcwd(temp, sizeof(temp));
+        printf("%s>>", temp);
+    	// printf(">>");
+        // scanf("%s",input); //fails due to spaces, tabs
 		getInput();
 
         if(strcmp(argval[0],"about")==0)
@@ -65,6 +96,18 @@ int main(int argc, char* argv[])
             char* foldername = argval[1];
             mkdir_func(foldername);
         }
-
+        else if(strcmp(argval[0],"exit")==0)
+        {
+            exitflag=1;
+        }
+        else if(strcmp(argval[0],"rmdir")==0)
+        {
+            char* foldername = argval[1];
+            rmdir_func(foldername);
+        }
+        else if(strcmp(argval[0],"clear")==0)
+        {
+            clear();
+        }
 	}
 }
