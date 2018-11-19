9904d2335ac5038b3b6d1d6bab708ed75d5719b5
PranjaliJain <pranjali.jain@iitgn.ac.in>
2018-11-19 07:39:44 +0530

    ls

--- Commands/ls.c
+++ Commands/ls.c
@@ -4,6 +4,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+//#include "./cd.c"
+//#include "./pwd.c"
 
 #define CYAN "\x1b[96m"
 #define GREEN "\x1b[92m"
@@ -35,11 +37,22 @@ void nameFile(struct dirent* name,char* followup)
 void ls_func(char* dirname)
 { 
 
+    
     int i=0;
     struct dirent **dir;
     struct stat sb;
     int numfiles = scandir(dirname, &dir, 0, alphasort);
     int j=0;
+
+
+    char pwdpath[1000];
+    char pathofpresent[1000];
+    pwd_func(pathofpresent,0);
+
+    
+    cd_func_for_ls(pwdpath,dirname);
+    
+    dirname=".";
     if (numfiles >= 0)
     {
         
@@ -48,26 +61,33 @@ void ls_func(char* dirname)
             if((strcmp(dir[i]->d_name,".")!=0 && strcmp(dir[i]->d_name,"..")!=0) && dir[i]->d_name[0]!='.')            
             {
 
+                
+
                 struct dirent* name=dir[i];
                 //char names[20]=dir[i]->d_name;
-                if(name->d_type == DT_REG)          // regular file
+                
+                if(name->d_type == DT_DIR)    // a directory
+                {
+                    printf("%s%s\t\t",BOLDBLUE, name->d_name);
+                }
+                else if (stat(dir[i]->d_name, &sb) == 0 && sb.st_mode & S_IXUSR)
+                {
+                    printf("%s%s\t\t",BOLDGREEN, name->d_name);
+                }
+                // if (access(dir[i]->d_name, F_OK|X_OK) == 0)
+                // {
+                //     printf("inside executable\n");
+                //     printf("%s%s\t\t",BOLDGREEN, name->d_name);
+                // }
+                else if(name->d_type == DT_REG)          // regular file
                 {
                     printf("%s\t\t", name->d_name);
                 }
-
                 else if(name->d_type == DT_CHR)    // a character device 
                 {
                     printf("%s%s\t\t",CYAN, name->d_name);
                 }
                 else if(name->d_type == DT_FIFO)    // a named pipe 
-                {
-                    printf("%s%s\t\t",BOLDGREEN, name->d_name);
-                }
-                else if(name->d_type == DT_DIR)    // a directory
-                {
-                    printf("%s%s\t\t",BOLDBLUE, name->d_name);
-                }
-                else if (stat(dir[i]->d_name, &sb) == 0 && sb.st_mode & S_IXUSR)
                 {
                     printf("%s%s\t\t",GREEN, name->d_name);
                 }
@@ -75,6 +95,8 @@ void ls_func(char* dirname)
                 {
                     printf("%s%s\t\t",BOLDCYAN, name->d_name);
                 }
+                
+                
                 j++;
             }
             
@@ -87,10 +109,12 @@ void ls_func(char* dirname)
     {
         perror("Empty directory\n");
     }
+    cd_func_for_ls(pwdpath,pathofpresent);
 
 }
 void ls_func_present(char* dirname)
 { 
+    
     int i=0;
     struct dirent **dir;
     struct stat sb;
